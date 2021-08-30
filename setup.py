526d192f499a233bc352dea579e249f49e8c2dd8
ewindels <etienne.windels@hec.edu>
2021-08-30 14:36:45 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="crosswords-coop",
+    version="0.0.1",
+    author="Etienne Windels",
+    author_email="etienne.windels@hec.edu",
+    description="Crosswords Coop App",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
