0c36dfce8c5b248f3d2a8383154fd3207f08c9bb
ewindels <etienne.windels@hec.edu>
2021-08-31 20:57:34 +0100

    Update test_parse.py

--- test/test_parse.py
+++ test/test_parse.py
@@ -5,4 +5,5 @@ from crosswords_coop import parse
 @pytest.mark.parametrize("file", os.listdir(os.path.join('data', 'crosswords')))
 
 def test_parsefile(file):
-    parse.parse(file)
+    grid_img_dict = parse.parse(file)
+    assert grid_img_dict
