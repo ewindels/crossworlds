513913753be2692ae4eb021f886934333f611026
ewindels <etienne.windels@hec.edu>
2021-09-12 16:53:57 +0100

    add tests

--- test/test_parse.py
+++ test/test_parse.py
@@ -1,9 +1,23 @@
 import os
+import json
 import pytest
 from crosswords_coop import parse
 
-@pytest.mark.parametrize("file", os.listdir(os.path.join('data', 'crosswords')))
+with open(os.path.join('data', 'content.json'), 'r') as fp:
+    content_dict = json.load(fp)
+    grids_with_size = [grid for grid in content_dict if 'size' in content_dict[grid]]
+    grids_with_text_cells = [grid for grid in content_dict if 'text' in content_dict[grid]]
 
-def test_parsefile(file):
-    grid_img_dict = parse.parse(file)
+@pytest.mark.parametrize("grid_file", os.listdir(os.path.join('data', 'crosswords')))
+def test_parse(grid_file):
+    grid_img_dict = parse.parse(grid_file)
     assert grid_img_dict
+
+@pytest.mark.parametrize("grid_file", grids_with_size)
+def test_check_size(grid_file):
+    grid_img_dict = parse.parse(grid_file)
+    length, width = content_dict[grid_file]['size']
+    predicted_length = max(l for l, w in grid_img_dict) + 1
+    predicted_width = max(w for l, w in grid_img_dict) + 1
+    assert length == predicted_length
+    assert width == predicted_width
