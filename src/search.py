f2d35390b9de7c7744ac3caa5b3af49468d85024
ewindels <etienne.windels@hec.edu>
2021-10-05 15:16:33 +0100

    add vocab initialization

--- src/search.py
+++ src/search.py
@@ -1,5 +1,7 @@
-from grid import Grid
+import os
+import json
 from copy import copy
+from grid import Grid
 
 def recursive_search(vocab_set: set[str], grid: Grid, found_grids: list[dict]) -> None:
     if grid.word_starts:
@@ -15,3 +17,11 @@ def recursive_search(vocab_set: set[str], grid: Grid, found_grids: list[dict]) -
             recursive_search(vocab_set, grid, found_grids)
             grid.remove_word(word_start, word)
             vocab_set.add(word)
+
+def init_vocab() -> set[str]:
+    vocab_set = set()
+    translations_dir = os.path.join('data', 'translations')
+    for vocab_json in os.listdir(translations_dir):
+        with open(os.path.join(translations_dir, vocab_json), 'r', encoding='utf-8') as fp:
+            vocab_set.update(json.load(fp))
+    return vocab_set
