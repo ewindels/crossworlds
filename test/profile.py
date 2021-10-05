80bac7b3cdb5bd966cd7d670eec3a6b2eefe2e75
ewindels <etienne.windels@hec.edu>
2021-10-05 23:47:20 +0100

    update profiling

--- test/profile.py
+++ test/profile.py
@@ -1,5 +1,12 @@
-# python -m cProfile -o test\profile_output test\profile.py
-# python -m pstats test\profile_output
-from crosswords_coop import parse
+# python -m cProfile -o test/profile_output test/profile.py
+# python -m pstats test/profile_output
+# sort time
+# stats 10
 
-parse.parse('crossword_0.jpg')
+from grid import Grid
+from search import recursive_search, init_vocab
+
+grid = Grid(3, 3)
+found_grids = []
+vocab = init_vocab()
+recursive_search(vocab, grid, found_grids)
