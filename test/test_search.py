e38e2262b5dc90298afeba0ec529b163f9c3002c
ewindels <ewindels@bloomberg.net>
2021-10-13 19:24:53 +0200

    Update test_search.py

--- test/test_search.py
+++ test/test_search.py
@@ -1,19 +1,19 @@
 import pytest
 from grid import Grid
-from search import recursive_search, init_vocab
+from search import get_full_grids
+from lookup import init_vocab
 
 
-@pytest.mark.parametrize("grid,vocab,expected", [
-    (Grid(2, 2), {'ab', 'bb'}, 2),
-    (Grid(2, 2), {'ab', 'dd'}, 0),
-    (Grid(2, 2), {'ab', 'bb', 'cb', 'dd'}, 6),
-    (Grid(3, 3), {'abc', 'bbb', 'ba', 'ca', 'ddd', 'dd'}, 2),
-    (Grid(3, 3), {'arc', 'cv', 'ara', 'a'}, 0),
-    (Grid(3, 3), {'are', 'ara', 'en', 'an'}, 2)
+@pytest.mark.parametrize("width,height,vocab,expected", [
+    (2, 2, {'ab', 'bb'}, 2),
+    (2, 2, {'ab', 'dd'}, 0),
+    (2, 2, {'ab', 'bb', 'cb', 'dd'}, 6),
+    (3, 3, {'abc', 'bbb', 'ba', 'ca', 'ddd', 'dd'}, 2),
+    (3, 3, {'arc', 'cv', 'ara', 'a'}, 0),
+    (3, 3, {'are', 'ara', 'en', 'an'}, 2)
 ])
-def test_search(grid, vocab, expected):
-    found_grids = []
-    recursive_search(vocab, grid, found_grids)
+def test_search(width, height, vocab, expected):
+    found_grids = get_full_grids(width, height, vocab)
     assert len(found_grids) == expected
 
 
@@ -22,8 +22,7 @@ def vocab():
     return init_vocab()
 
 
-@pytest.mark.parametrize("grid", [Grid(2, 2), Grid(3, 3)])
-def test_vocab_search(grid, vocab):
-    found_grids = []
-    recursive_search(vocab, grid, found_grids)
+@pytest.mark.parametrize("width,height", [(2, 2), (3, 3)])
+def test_vocab_search(width, height, vocab):
+    found_grids = get_full_grids(width, height, vocab)
     assert found_grids
