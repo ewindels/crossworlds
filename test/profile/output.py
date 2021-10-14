1ee3c022cd95753ee8be8222c20d26beb8d0bb1f
ewindels <ewindels@bloomberg.net>
2021-10-14 09:16:49 +0200

    add output script

---
+++ test/profile/output.py
@@ -0,0 +1,12 @@
+from search import get_full_grids
+from lookup import init_vocab
+
+found_grids = get_full_grids(4, 4, init_vocab())
+output_str = ''
+for word_patterns, grid_str in found_grids:
+    output_str += grid_str
+    for word_pattern, word in word_patterns.items():
+        output_str += f'{str(word_pattern)}: {word} | '
+    output_str += '\n---------\n'
+with open('test/profile/output.txt', 'w') as fp:
+    fp.write(output_str)
