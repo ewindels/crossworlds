b704bcbcdb7ae93655bd327d9f6bbc467d92773c
ewindels <ewindels@bloomberg.net>
2021-10-15 14:08:02 +0200

    update data

--- test/output/output.py
+++ test/output/output.py
@@ -1,9 +1,9 @@
 from search import get_full_grids
 from lookup import init_vocab
 
-
+vocab = init_vocab()
 for width, height in [(2, 2), (3, 3), (4, 4)]:
-    found_grids = get_full_grids(width, height, init_vocab())
+    found_grids = get_full_grids(width, height, vocab)
     output_str = ''
     for word_patterns, grid_str in found_grids:
         output_str += grid_str
@@ -11,5 +11,5 @@ for width, height in [(2, 2), (3, 3), (4, 4)]:
             output_str += f'{str(word_pattern)}: {word} | '
         output_str += '\n---------\n'
     print(f'{width}x{height}: {len(found_grids)} grids')
-    with open(f'test/profile/output_{width}x{height}.txt', 'w') as fp:
+    with open(f'test/output/output_{width}x{height}.txt', 'w') as fp:
         fp.write(output_str)
