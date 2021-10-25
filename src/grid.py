20714e4022c6b7067fab47c9d10c6ad1e28ec0f3
ewindels <ewindels@bloomberg.net>
2021-10-25 23:03:16 +0200

    more restrictions

--- src/grid.py
+++ src/grid.py
@@ -11,7 +11,8 @@ class Grid:
             if col % 2:
                 return False
             elif (
-                    (1, col - 2) in self.values
+                    col < 4
+                    or (1, col - 2) in self.values
                     or (1, col - 4) in self.values
                     or (3, col) in self.values
                     or (2, col - 1) in self.values
@@ -21,7 +22,8 @@ class Grid:
             if row % 2:
                 return False
             elif (
-                    (row - 2, 1) in self.values
+                    row < 4
+                    or (row - 2, 1) in self.values
                     or (row - 4, 1) in self.values
                     or (row, 3) in self.values
                     or (row - 1, 2) in self.values
@@ -130,15 +132,15 @@ class Grid:
                 switchable_values.extend([(self.height - 1, col), (self.height - 2, col)])
             switchable_values.extend([(self.height - 3, self.width - 1),
                                       (self.height - 3, self.width - 2)])
-            if self.height % 2 == 0:
-                switchable_values.append((self.height - 2, 1))
+            if self.height % 2 == 0 and self.height > 6:
+                switchable_values.append((self.height - 6, 1))
         elif direction == 'horizontal':
             for row in range(self.height - 3, 1, -1):
                 switchable_values.extend([(row, self.width - 1), (row, self.width - 2)])
             switchable_values.extend([(self.height - 1, self.width - 3),
                                       (self.height - 2, self.width - 3)])
-            if self.width % 2 == 0:
-                switchable_values.append((1, self.width - 2))
+            if self.width % 2 == 0 and self.width > 6:
+                switchable_values.append((1, self.width - 6))
         return switchable_values
 
     def find_valid_grids(self, switchable_values: list[Coor], valid_grids: list[set[Coor]]) -> None:
