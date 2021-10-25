af0b32317b585a7558d3064c62706f69264db1d1
ewindels <ewindels@bloomberg.net>
2021-10-25 18:11:23 +0200

    add sizes

--- src/create_grids.py
+++ src/create_grids.py
@@ -1,9 +1,10 @@
+import json
 from grid import Grid
 
 
 def main():
     for height in range(5, 8):
-        for width in range(height, 9):
+        for width in range(height, 8):
             for expansion in ['horizontal', 'vertical']:
                 new_grids = []
                 with open(f'output/{height}x{width}.grids', 'r') as fp:
@@ -13,9 +14,13 @@ def main():
 
                 new_height = height + (expansion == 'vertical')
                 new_width = width + (expansion == 'horizontal')
-                print(new_height, new_width)
                 with open(f'output/{new_height}x{new_width}.grids', 'w') as fp:
                     fp.write(Grid.to_str(new_grids))
+                with open(f'output/sizes.json', 'r') as fp:
+                    size_dict = json.load(fp)
+                size_dict[f'{new_height}x{new_width}'] = len(new_grids)
+                with open(f'output/sizes.json', 'w') as fp:
+                    json.dump(size_dict, fp, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
