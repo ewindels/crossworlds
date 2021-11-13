cdd88e6ec677df599843b01fa77c564c0ca4a6ad
ewindels <ewindels@bloomberg.net>
2021-11-13 12:17:02 +0000

    comment out output temporarily

--- src/crossworlds/create_word_grids.py
+++ src/crossworlds/create_word_grids.py
@@ -8,16 +8,16 @@ def main():
     width = 4
     definitions = {}
     vocab = init_vocab()
-    found_grids = sorted(get_full_grids(height, width, definitions, vocab))
+    found_grids = list(get_full_grids(height, width, definitions, vocab))
     print(f'Found {len(found_grids)} grids')
-    found_grids_str = ''.join(found_grids)
-    with open(f'output/word_grids/{height}x{width}.wordgrids', 'w', encoding='utf-8') as fp:
-        fp.write(found_grids_str)
-    with open('output/word_grids/sizes.json', 'r', encoding='utf-8') as fp:
-        size_dict = json.load(fp)
-    size_dict[f'{height}x{width}'] = len(found_grids)
-    with open('output/word_grids/sizes.json', 'w', encoding='utf-8') as fp:
-        json.dump(size_dict, fp, indent=4, sort_keys=True)
+    # found_grids_str = ''.join(found_grids)
+    # with open(f'output/word_grids/{height}x{width}.wordgrids', 'w', encoding='utf-8') as fp:
+    #     fp.write(found_grids_str)
+    # with open('output/word_grids/sizes.json', 'r', encoding='utf-8') as fp:
+    #     size_dict = json.load(fp)
+    # size_dict[f'{height}x{width}'] = len(found_grids)
+    # with open('output/word_grids/sizes.json', 'w', encoding='utf-8') as fp:
+    #     json.dump(size_dict, fp, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
