17b74fdf3d9568f7718f52e6d8deb70d2f4db474
ewindels <ewindels@bloomberg.net>
2021-11-13 11:10:29 +0000

    increase profile size

--- profile/word_grid/profile.sh
+++ profile/word_grid/profile.sh
@@ -1,1 +1,1 @@
-python -m cProfile -s tottime profile/word_grid/profile.py | head -15 > profile/word_grid/profile.txt
+python -m cProfile -s tottime profile/word_grid/profile.py | head -20 > profile/word_grid/profile.txt
